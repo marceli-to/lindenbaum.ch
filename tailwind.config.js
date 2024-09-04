e656095f5216f0e07442984e79ec87ff57c689f1
Marcel Stadelmann <m@marceli.to>
2024-09-04 15:29:45 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,6 @@ export default {
       },
 
       backgroundImage: {
-        'marker': "url('../img/marker.svg')",
         'dropdown': "url(\"data:image/svg+xml,%3Csvg width='18' height='12' viewBox='0 0 18 12' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M16.7 1.84095L9.20001 9.39999L1.70001 1.84095' stroke='%23dedf15' stroke-width='2'/%3E%3C/svg%3E\")",
       },
 
