2e62e0d19dc4f016fbc87b27112267992b484b09
Marcel Stadelmann <m@marceli.to>
2024-10-01 11:00:04 +0200

    wip

--- resources/js/forms/contact/CooperativeRequestForm.vue
+++ resources/js/forms/contact/CooperativeRequestForm.vue
@@ -36,7 +36,7 @@
       />
     </form-group>
     <form-group>
-      <form-label id="street" label="Strasse" required />
+      <form-label id="street" label="Strasse/Nr." required />
       <form-text-field 
         v-model="form.street" 
         :error="errors.street"
@@ -44,7 +44,7 @@
       />
     </form-group>
     <form-group>
-      <form-label id="location" label="Ort" required />
+      <form-label id="location" label="PLZ/Ort" required />
       <form-text-field 
         v-model="form.location" 
         :error="errors.location"
