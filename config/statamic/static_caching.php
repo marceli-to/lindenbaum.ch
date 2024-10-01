b10859bb00282e0f3ff04d9e43bca421373620da
Marcel Stadelmann <m@marceli.to>
2024-10-01 13:36:46 +0200

    wip

--- config/statamic/static_caching.php
+++ config/statamic/static_caching.php
@@ -80,23 +80,23 @@ return [
         'class' => null,
 
         'rules' => [
-          // 'collections' => [
-          //   'pages' => [
-          //     'urls' => [
-          //       '/*',
-          //     ]
-          //   ],
-          //   'news' => [
-          //     'urls' => [
-          //       '/*',
-          //     ]
-          //   ],
-          //   'projects' => [
-          //     'urls' => [
-          //       '/',
-          //     ]
-          //   ],
-          // ],
+          'collections' => [
+            'pages' => [
+              'urls' => [
+                '/*',
+              ]
+            ],
+            'jobs' => [
+              'urls' => [
+                '/*',
+              ]
+            ],
+            'events' => [
+              'urls' => [
+                '/',
+              ]
+            ],
+          ],
         ],
 
     ],
